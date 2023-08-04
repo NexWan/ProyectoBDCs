378d2944e53eaf18ba06a8e28ad1842f5575dda0
NexWan <l21051403@saltillo.tecnm.mx>
2023-08-04 12:04:25 -0600

    Add project files.

---
+++ WpfApp1/ConnectionObj.cs
@@ -0,0 +1,35 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class ConnectionObj
+    {
+        private OracleConnection conn;
+
+        public ConnectionObj() {
+            conn = new OracleConnection();
+        }
+
+        public OracleConnection getConnection()
+        {
+            try
+            {
+                conn.ConnectionString = "Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)" +
+                    "(PORT=1521)))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=xe)))" +
+                    ";User Id=prueba;Password=prueba;";
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                conn = null;
+            }
+            return conn;
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Oracle.ManagedDataAccess.Types;
+using Oracle.ManagedDataAccess.Client;
+using System.Diagnostics;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();           
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OracleConnection conn = new ConnectionObj().getConnection();
+
+            conn.Open();
+
+            OracleCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "SELECT * FROM tab";
+
+            OracleDataReader reader = cmd.ExecuteReader();
+            if (reader.Read())
+            {
+                Modificar.Content = reader.GetString(0);
+            }
+            
+        }
+    }
+}
